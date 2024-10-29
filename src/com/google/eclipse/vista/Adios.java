69453d3762573a5d33e49f1b5f6715ea851cd0a2
Amador Tejada <atejcas871@g.educaand.es>
2024-10-29 09:53:03 +0100

    nueva clase adios

---
+++ src/com/google/eclipse/vista/Adios.java
@@ -0,0 +1,9 @@
+package com.google.eclipse.vista;
+
+public class Adios {
+	public static void main (string[] args) {
+	
+	System.out.println("Adios");
+	}
+
+}
--- src/proyectoeclipse/Hola.java
+++ src/proyectoeclipse/Hola.java
@@ -5,9 +5,11 @@ public class Hola {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo!!!!!!!");
+		
 	}
   
 }
+
 
 
 
